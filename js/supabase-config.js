81ac5bd008da1c61ba37c26460c277bfc5ce08d5
maememecoin-hub <maememecoin@gmail.com>
2026-04-09 22:17:09 +0200

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,6 @@
-// Dane znajdziesz w Supabase: Project Settings -> API
+// Dane konfiguracyjne
 const SUPABASE_URL = 'https://yifocbciyywcyuwdvsku.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpZm9jYmNpeXl3Y3l1d2R2c2t1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NTMwMTksImV4cCI6MjA5MTMyOTAxOX0.CNksc1prtEKnLXCpJg6-tko_q0P2TwomDfTHJrLGP-4';
 
-const supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Inicjalizacja klienta
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
